dd01662a3b4c40f08060938554f8805b2190beb4
martial-manu <70874605+martial-manu@users.noreply.github.com>
2021-12-28 16:55:49 +0530

    Create missing coin sum.cpp

---
+++ missing coin sum.cpp
@@ -0,0 +1,56 @@
+// हौसला दिलाएगा मंज़िल  
+ 
+#include<bits/stdc++.h>
+ 
+using namespace std;
+#define int long long
+const int mod = 998244353;
+#define F first
+#define S second
+#define pb push_back
+ 
+const int N = 2e5+1;
+ 
+ 
+template<typename... T>
+void debug(T&... args){
+  cout << "\n==================\n";
+  ((cout<< args<<", "), ...);
+  cout << "\n==================\n";
+}
+template<typename T>
+void debug(vector<T>& a){
+  cout << "\n==============\n";
+  for(auto& e:a)cout << e << ' ';
+  cout << "\n==============\n";
+}
+ 
+template<typename... T>
+void put(T&... args) { ((cout << args << " "), ...);}
+
+int clc(int og , vector<int>& p){
+  int cost = 0;
+  for(auto& e:p)cost = cost + abs(og - e);
+  return cost ;
+}
+
+void solve(){
+  int n; cin>> n ; vector<int>x(n); for(auto& e:x)cin >> e;
+  sort(x.begin() , x.end());
+  int ans = 1;
+  for(int i =0 ; i<n; i++){
+      if(x[i]<= ans){
+        ans += x[i];
+      }
+      else{
+        cout << ans << '\n';return;
+      }
+  }
+  cout << ans << '\n';
+}
+ 
+signed main(){
+  // ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+  // int t; cin>> t; while(t--)
+  solve();
+}
